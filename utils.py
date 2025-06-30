2cd9eafff091ca3e96a3b60e4cca07e085fe5c9c
vatsal9876 <vatsal.gupta@iitg.ac.in>
2025-06-30 14:11:43 +0530

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def dice_coef(y_true, y_pred, smooth=1e-6):
+    y_true_f = y_true.flatten()
+    y_pred_f = y_pred.flatten()
+    intersection = np.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (np.sum(y_true_f) + np.sum(y_pred_f) + smooth)
+
+def vessel_density(mask):
+    return np.mean(mask > 0.5)
